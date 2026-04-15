2509be98dc49bcac6069f699033e5a743065301e
Goldmensch <pm@goldmensch.dev>
2026-04-15 12:59:54 +0200

    Release 0.1 : Initial release

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jreleaser.version.SemanticVersion
 allprojects {
   group = "dev.goldmensch"
 
-  version = "0.1.0-SNAPSHOT"
+  version = "0.1"
   if (System.getenv("DEPLOY_ACTIVE") == "SNAPSHOT") {
     if (!version.toString().endsWith("-SNAPSHOT")) {
       val semver = SemanticVersion.of(version.toString())
